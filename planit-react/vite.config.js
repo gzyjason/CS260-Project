e9a010cb99fcd533d72f90977be4008ca315c93b
gzyjason <46142323+gzyjason@users.noreply.github.com>
2025-10-13 21:50:21 -0600

    Edited multiple files as I try to fix styling issues.

--- planit-react/vite.config.js
+++ planit-react/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // <-- Import the plugin
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
-});
+    plugins: [react(), tailwindcss()], // <-- Add it to the plugins array
+})
