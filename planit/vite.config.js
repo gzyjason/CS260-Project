f8f19017712a9c9e525688ff29fb4fa37f85554d
gzyjason <46142323+gzyjason@users.noreply.github.com>
2025-11-04 10:19:57 -0700

    Initial setup for services

--- planit/vite.config.js
+++ planit/vite.config.js
@@ -4,5 +4,10 @@ import tailwindcss from '@tailwindcss/vite' // <-- Import the plugin
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    server: {
+        proxy: {
+            '/api': 'http://localhost:4000',
+        },
+    },
     plugins: [react(), tailwindcss()], // <-- Add it to the plugins array
 })
