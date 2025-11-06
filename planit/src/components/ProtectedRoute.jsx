3a294dd87dd9a871116ea0af4d139f0ef372b09b
gzyjason <46142323+gzyjason@users.noreply.github.com>
2025-11-05 21:44:06 -0700

    added new component to redirect user to calendar page if logged in.

---
+++ planit/src/components/ProtectedRoute.jsx
@@ -0,0 +1,22 @@
+// planit/src/components/ProtectedRoute.jsx
+
+import { useAppContext } from '../hooks/useAppContext';
+import { Navigate } from 'react-router-dom';
+
+/**
+ * This component protects routes that require a user to be logged in.
+ * It checks the 'userName' from our context.
+ */
+const ProtectedRoute = ({ children }) => {
+    const { userName } = useAppContext();
+
+    if (userName === 'Guest') {
+        // If user is 'Guest', redirect them to the login page ("/")
+        return <Navigate to="/" replace />;
+    }
+
+    // If user is logged in (e.g., 'jason@byu.edu'), show the page
+    return children;
+};
+
+export default ProtectedRoute;
